4fb6f8138f9fd20c0fd2cae720b12b492145154b
José María Valera Reales <chemaclass@outlook.es>
2014-04-12 17:35:30 +0200

    Añado args al main

--- src/main.go
+++ src/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"even"
+	"flag"
+	"fmt"
 	"params"
 	"sum"
 )
@@ -11,4 +13,13 @@ func main() {
 	params.Main()
 	even.Main(1, 2, 72)
 	sum.Main(1, 2, 3, 4, 5)
+	//Args
+	flag.Parse()
+	a := flag.Args()
+	for _, i := range a {
+		fmt.Printf("> %s\n", i)
+	}
+	fmt.Printf(">> %s", flag.Arg(0))
+	//Args end
+
 }
