07f03637cf5aba70733a4607b4fffefe5f147ee0
José María Valera Reales <chemaclass@outlook.es>
2014-04-12 15:44:39 +0200

    Actualizo el main

--- src/main.go
+++ src/main.go
@@ -1,51 +1,12 @@
-package src
+package main
 
 import (
 	"fmt"
-	"strconv"
+	"params"
 )
 
-type Seq struct {
-	nombre string
-	act    int
-	salto  int
-}
-
-func intSeq(p ...string) func() (string, int) {
-
-	var act, salto int
-	var nombre string
-
-	switch len(p) {
-	case 0:
-		nombre, act, salto = "Seq", 0, 1
-	case 1:
-		nombre, act, salto = p[0], 0, 1
-	case 2:
-		t, _ := strconv.Atoi(p[1])
-		nombre, act, salto = p[0], t, 1
-	default:
-		t, _ := strconv.Atoi(p[1])
-		t2, _ := strconv.Atoi(p[2])
-		nombre, act, salto = p[0], t, t2
-	}
-
-	seq := Seq{nombre, act, salto}
-
-	return func() (string, int) {
-		seq.act += seq.salto
-		return seq.nombre, seq.act
-	}
-}
-
 func main() {
-	nextInt := intSeq("Mi Seq1", "1", "2")
-
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-
-	newInts := intSeq("Sec-2", "3")
-	fmt.Println(newInts())
-
+	fmt.Println("main BEGIN")
+	params.Main()
+	fmt.Println("main END")
 }
