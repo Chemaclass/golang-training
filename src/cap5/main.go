6092023da085bf5d544d98ba558f6c8d8a2768ba
José María Valera Reales <chemaclass@outlook.es>
2014-04-13 20:29:25 +0200

    Añado cap5

---
+++ src/cap5/main.go
@@ -0,0 +1,20 @@
+package cap5
+
+import (
+	"fmt"
+)
+
+/**
+ * Capitulo 5 de Learning Go
+ */
+func main() {
+	var p *int
+	fmt.Printf("%v", p)
+	var i int
+	p = &i
+	fmt.Printf("$v", p)
+}
+
+func Main() {
+	main()
+}
