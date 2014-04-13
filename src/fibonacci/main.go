10494e3f28ea265db967e67db2d6f8a9e540a3b8
José María Valera Reales <chemaclass@outlook.es>
2014-04-13 19:47:37 +0200

    añado Map(fn,arr)

--- src/fibonacci/main.go
+++ src/fibonacci/main.go
@@ -19,8 +19,20 @@ func printFibonacci(n int) {
 	}
 }
 
+func Map(f func(int) int, l []int) []int {
+	j := make([]int, len(l))
+	for k, v := range l {
+		j[k] = f(v)
+	}
+	return j
+}
+
 func main() {
-	printFibonacci(10)
+	m := []int{1,2,3}
+	f := func(i int) int {
+		return i * i
+	}
+	fmt.Printf("%v", Map(f,m))
 }
 
 func Main() {
