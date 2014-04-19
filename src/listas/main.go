0aac8e20d0580d88c08fee77bfe374918fe15b1e
José María Valera Reales <chemaclass@outlook.es>
2014-04-19 23:58:55 +0200

    podemos copiar una lista con copy(destino,origen)

--- src/listas/main.go
+++ src/listas/main.go
@@ -5,11 +5,25 @@ import (
 )
 
 func main() {
+	e2()
+}
+
+// Copy slice
+func e2() {
+	slice1 := []int{1, 2, 3}
+	slice2 := make([]int, 2)
+	copy(slice2, slice1)
+	fmt.Println(slice1, slice2)
+}
+
+//append slice
+func e1() {
 	fmt.Println("listas.main")
 	slice1 := []int{1, 2, 3}
 	slice2 := append(slice1, 4, 5)
 	fmt.Println(slice1, slice2)
 }
+
 func Main() {
 	fmt.Println("listas.Main")
 	main()
