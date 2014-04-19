3a4f8035950b436fdc43766b8ec81e3812da2318
José María Valera Reales <chemaclass@outlook.es>
2014-04-20 00:42:47 +0200

    creo func media para obtener la media de un array de números

--- src/funciones/main.go
+++ src/funciones/main.go
@@ -5,7 +5,21 @@ import (
 )
 
 func main() {
+	e1()
 
+}
+
+func e1() {
+	media := func(xs []float64) float64 {
+		total := 0.0
+		for _, v := range xs {
+			total += v
+		}
+		return total / float64(len(xs))
+	}
+
+	xs := []float64{98, 93, 77, 82, 83}
+	fmt.Printf("Media %.3f", media(xs))
 }
 
 func Main() {
