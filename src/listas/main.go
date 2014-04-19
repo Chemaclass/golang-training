78835d182b4c76e82bc5b1ed06e37f73d8bf7225
José María Valera Reales <chemaclass@outlook.es>
2014-04-19 23:52:04 +0200

    creo dir listas

---
+++ src/listas/main.go
@@ -0,0 +1,16 @@
+package listas
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("listas.main")
+	slice1 := []int{1, 2, 3}
+	slice2 := append(slice1, 4, 5)
+	fmt.Println(slice1, slice2)
+}
+func Main() {
+	fmt.Println("listas.Main")
+	main()
+}
--- src/main.go
+++ src/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"commands"
-	"even"
-	"fibonacci"
-	"flag"
 	"fmt"
-	"obj1"
-	"params"
-	"sum"
+	"listas"	
 )
 
 func main() {
-	doScanExcample()
+	listas.Main()
 }
 
 func doScanExcample() {
@@ -20,20 +14,4 @@ func doScanExcample() {
 	fmt.Print("What's your name? ")
 	fmt.Scanf("%s", &name)
 	fmt.Printf("Hello %s\n", name)
-}
-
-func act1() {
-	params.Main()
-	even.Main(1, 2, 72)
-	sum.Main(1, 2, 3, 4, 5)
-	//Args
-	flag.Parse()
-	a := flag.Args()
-	for _, i := range a {
-		fmt.Printf("> %s\n", i)
-	}
-	//Args end
-	obj1.Main()
-	commands.Main()
-	fibonacci.Main()
 }
