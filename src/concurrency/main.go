d3050208b10735258e3d9ba90bdc1e23864219bd
José María Valera Reales <chemaclass@outlook.es>
2014-04-20 14:28:38 +0200

    concurrencia básica

--- src/concurrency/main.go
+++ src/concurrency/main.go
@@ -2,18 +2,31 @@ package concurrency
 
 import (
 	"fmt"
+	"time"
+	"math/rand"
 )
 
 func main() {
 	e1()
 }
 
-
-func e1(){
-
+func e1() {
+	for i:= 0; i< 10; i++{
+		go f(i)
+	}
+	var input string
+	fmt.Scanln(&input)
 }
 
-func Main(){
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+func Main() {
 	fmt.Println("concurrency.Main")
 	main()
 }
