4019abccf6b00632835723a23269dc93869f2705
José María Valera Reales <chemaclass@outlook.es>
2014-04-20 16:40:03 +0200

    probamos select para canales

--- src/concurrency/main.go
+++ src/concurrency/main.go
@@ -7,21 +7,52 @@ import (
 )
 
 func main() {
-	e2()
+	e3()
 }
 
+//Select
+func e3() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		for {
+			c1 <- "from 1"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+	go func() {
+		for {
+			c2 <- "from 2"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println("<-c1|", msg1)
+			case msg2 := <-c2:
+				fmt.Println("<-c2|", msg2)
+			}
+		}
+	}()
+	var input string
+	fmt.Scanln(&input)
+}
+
+//Pin-pon
 func e2() {
-	pinger := func(c chan <- string) {
+	pinger := func(c chan<- string) {
 		for i := 0; ; i++ {
 			c <- "ping"
 		}
 	}
-	ponger := func(c chan <- string) {
-		for i:=0;; i++ {
+	ponger := func(c chan<- string) {
+		for i := 0; ; i++ {
 			c <- "pong"
 		}
 	}
-	printer := func(c <- chan string) {
+	printer := func(c <-chan string) {
 		for {
 			msg := <-c
 			fmt.Println(msg)
