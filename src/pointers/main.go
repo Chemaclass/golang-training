b64a46bbb6af2b6a12272be97db36686945e4eb2
José María Valera Reales <chemaclass@outlook.es>
2014-04-20 02:02:18 +0200

    Lo básico con punteros

--- src/pointers/main.go
+++ src/pointers/main.go
@@ -9,7 +9,17 @@ func main() {
 }
 
 func e1() {
-
+	zero := func(p int) {
+		p = 0
+	}
+	pzero := func(p *int) {
+		*p = 0
+	}
+	x := 5
+	zero(x) // No modifica x
+	fmt.Printf("x es %d|", x)
+	pzero(&x)// sí modifica x
+	fmt.Printf("x es %d\n", x)
 }
 
 func Main() {
