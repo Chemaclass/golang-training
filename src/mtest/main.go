66bfe8d552421c92ad5c9160ddaf0e872777f174
José María Valera Reales <chemaclass@outlook.es>
2014-04-20 17:58:01 +0200

    Creo unos tests para el max y min dada una lista

--- src/mtest/main.go
+++ src/mtest/main.go
@@ -4,33 +4,71 @@ import "testing"
 import "fmt"
 
 type testPair struct {
-	values  []float64
-	average float64
+	values    []float64
+	resultado float64
 }
 
-var tests = []testPair{
+type testTipo struct {
+	tipo     string
+	testPair []testPair
+}
+
+var tests = []testTipo{
+	testTipo{"media", testsMedia},
+	testTipo{"max", testsMax},
+	testTipo{"min", testsMin},
+}
+
+var testsMedia = []testPair{
 	{[]float64{1, 2}, 1.5},
 	{[]float64{1, 1, 1, 1, 1, 1}, 1},
 	{[]float64{1, 1}, 0}, //FAIL
 	{[]float64{1, -1}, 0},
+	{[]float64{}, 0},
+}
+var testsMax = []testPair{
+	{[]float64{1, 2}, 2},
+	{[]float64{5, 4}, 5},
+	{[]float64{3, 7}, 3}, // FAIL
+}
+var testsMin = []testPair{
+	{[]float64{1, 2}, 1},
+	{[]float64{5, 4}, 4},
+	{[]float64{3, 7}, 7}, // FAIL
 }
 
-func TestAverage(t *testing.T) {
-	for _, pair := range tests {
-		v := Average(pair.values)
-		if v != pair.average {
-			msg := fmt.Sprintln("For", pair.values,
-				"expected", pair.average,
-				"got", v)
-			fmt.Print(msg)
-			t.Error(msg)
-		} else {
-			fmt.Printf("ok\n")
+//Test Media
+func Tests(t *testing.T) {
+	for _, testsList := range tests {
+		for _, pair := range testsList.testPair {
+
+			var v float64
+			switch testsList.tipo {
+			case "media":
+				v = Media(pair.values)
+			case "max":
+				v = Max(pair.values)
+			case "min":
+				v = Min(pair.values)
+			}
+			if v != pair.resultado {
+				msg := fmt.Sprintln(testsList.tipo, "For", pair.values,
+					"expected", pair.resultado,
+					"got", v)
+				fmt.Print(msg)
+				t.Error(msg)
+			} else {
+				fmt.Printf("ok\n")
+			}
 		}
 	}
 }
 
-func Average(v []float64) float64 {
+//Devuelve la media de una lista
+func Media(v []float64) float64 {
+	if len(v) <= 0 {
+		return 0
+	}
 	var t float64 = 0
 	for _, v := range v {
 		t += v
@@ -38,11 +76,40 @@ func Average(v []float64) float64 {
 	return t / float64(len(v))
 }
 
+//Devuelve el máximo de una lista
+func Max(lista []float64) (m float64) {
+	if len(lista) <= 0 {
+		return 0
+	}
+	m = lista[0]
+	for _, v := range lista {
+		if v > m {
+			m = v
+		}
+	}
+	return
+}
+
+//Devuelve el menor de una lista
+func Min(lista []float64) (m float64) {
+	if len(lista) <= 0 {
+		return 0
+	}
+	m = lista[0]
+	for _, v := range lista {
+		if v < m {
+			m = v
+		}
+	}
+	return
+}
+
 func main() {
-	TestAverage(new(testing.T))
+	t := new(testing.T)
+	Tests(t)
 }
 
 func Main() {
 	fmt.Println("test.Main")
 	main()
- }
+}
