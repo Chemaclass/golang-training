d2b91439470ed12d79696a23fc58227765c2f145
José María Valera Reales <chemaclass@outlook.es>
2014-04-20 14:41:51 +0200

    Añado concurrencia mediante canales básica: pinger(c); printer(c)

--- src/concurrency/main.go
+++ src/concurrency/main.go
@@ -2,16 +2,39 @@ package concurrency
 
 import (
 	"fmt"
-	"time"
 	"math/rand"
+	"time"
 )
 
 func main() {
-	e1()
+	e2()
+}
+
+func e2() {
+	pinger := func(c chan string) {
+		for i := 0; ; i++ {
+			c <- "ping"
+		}
+	}
+	printer := func(c chan string) {
+		for {
+			msg := <-c
+			fmt.Println(msg)
+			time.Sleep(time.Second * 1)
+		}
+	}
+
+	c := make(chan string)
+	go pinger(c)
+	go printer(c)
+
+	var input string
+	fmt.Scanln(&input)
+
 }
 
 func e1() {
-	for i:= 0; i< 10; i++{
+	for i := 0; i < 10; i++ {
 		go f(i)
 	}
 	var input string
